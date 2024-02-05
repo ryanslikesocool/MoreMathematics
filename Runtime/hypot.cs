1bb1d5f8a9f40dd4112c157355e9236905541dbe
Ryan Boyer <ryanjboyer@gmail.com>
2024-02-05 04:46:26 -0700

    tweak docs

--- Runtime/hypot.cs
+++ Runtime/hypot.cs
@@ -6,14 +6,14 @@ namespace Unity.Mathematics {
 		// MARK: - float
 
 		/// <summary>
-		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two vectors define.
+		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two scalars define.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
 		public static float hypot(float x, float y)
 			=> sqrt(square(x) + square(y));
 
 		/// <summary>
-		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two vectors define.
+		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that a vector defines.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
 		public static float hypot(in float2 v)
@@ -22,14 +22,14 @@ namespace Unity.Mathematics {
 		// MARK: - double
 
 		/// <summary>
-		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two vectors define.
+		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two scalars define.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
 		public static double hypot(double x, double y)
 			=> sqrt(square(x) + square(y));
 
 		/// <summary>
-		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that two vectors define.
+		/// Returns the hypotenuse of a right-angled triangle with the sides that are adjacent to the right angle that a vector defines.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
 		public static double hypot(in double2 v)
