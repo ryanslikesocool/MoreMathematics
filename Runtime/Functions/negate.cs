9394e3d5040a1927d9cf9145504c39518410d3b9
Ryan Boyer <ryanjboyer@gmail.com>
2024-11-26 01:57:21 -0700

    Add `negate` overload with per-channel mask

--- Runtime/Functions/negate.cs
+++ Runtime/Functions/negate.cs
@@ -27,6 +27,19 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static float2 negate(this ref float2 x, in bool2 mask) {
+			for (int i = 0; i < 2; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static float3 negate(this ref float3 x) {
 			x = -x;
 			return x;
@@ -36,8 +49,34 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static float3 negate(this ref float3 x, in bool3 mask) {
+			for (int i = 0; i < 3; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static float4 negate(this ref float4 x) {
 			x = -x;
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
+		public static float4 negate(this ref float4 x, in bool4 mask) {
+			for (int i = 0; i < 4; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
 			return x;
 		}
 
@@ -65,6 +104,19 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static double2 negate(this ref double2 x, in bool2 mask) {
+			for (int i = 0; i < 2; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static double3 negate(this ref double3 x) {
 			x = -x;
 			return x;
@@ -74,8 +126,34 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static double3 negate(this ref double3 x, in bool3 mask) {
+			for (int i = 0; i < 3; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static double4 negate(this ref double4 x) {
 			x = -x;
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
+		public static double4 negate(this ref double4 x, in bool4 mask) {
+			for (int i = 0; i < 4; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
 			return x;
 		}
 
@@ -103,6 +181,19 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static int2 negate(this ref int2 x, in bool2 mask) {
+			for (int i = 0; i < 2; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static int3 negate(this ref int3 x) {
 			x = -x;
 			return x;
@@ -112,8 +203,34 @@ namespace Unity.Mathematics {
 		/// Replaces the given value with its additive inverse.
 		/// </summary>
 		[MethodImpl(AggressiveInlining)]
+		public static int3 negate(this ref int3 x, in bool3 mask) {
+			for (int i = 0; i < 3; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
 		public static int4 negate(this ref int4 x) {
 			x = -x;
+			return x;
+		}
+
+		/// <summary>
+		/// Replaces the given value with its additive inverse.
+		/// </summary>
+		[MethodImpl(AggressiveInlining)]
+		public static int4 negate(this ref int4 x, in bool4 mask) {
+			for (int i = 0; i < 4; i++) {
+				if (mask[i]) {
+					x[i] = -x[i];
+				}
+			}
 			return x;
 		}
 	}
